be4ec1d97a6108227e222f9ce272aa0a94a34d92
xaviarazor <prietojavi551@gmail.com>
2021-08-28 20:32:56 -0500

    gps-sys

--- App/app/src/main/java/com/thefourbotmen/hackmty/MainActivity.java
+++ App/app/src/main/java/com/thefourbotmen/hackmty/MainActivity.java
@@ -1,12 +1,19 @@
 package com.thefourbotmen.hackmty;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,12 +21,27 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.view.Window;
+
 import com.thefourbotmen.hackmty.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
+
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+
+
+    private LocationManager locationManager;
+
+    private Position usrLocation;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,4 +84,120 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+        final Window root = this.getWindow();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && this.checkSelfPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && this.checkSelfPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    112);
+            return;
+        }
+        locationManager = (LocationManager) root.getContext().getSystemService(
+                Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0,
+                0, this
+        );
+        Criteria ct=new Criteria();
+        ct.setAccuracy(Criteria.ACCURACY_COARSE);
+        locationManager.requestSingleUpdate(ct,this, Looper.myLooper());
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        usrLocation = new Position();
+        usrLocation.set(location.getLongitude(),
+                location.getLatitude());
+        Log.d("Location", String.valueOf(usrLocation));
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+    class Position {
+
+        private double x,y;
+
+        public Position() {
+            this.x = this.y = 0;
+        }
+
+        public Position(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Position subtraction(Position p){
+            Position res=null;
+            if(p!=null){
+                res=new Position(this.x-p.x,
+                        this.y-p.y);
+            }
+            return res;
+
+        }
+
+        public Position addition(Position p){
+            Position res=null;
+            if(p!=null){
+                res=new Position(this.x+p.x,
+                        this.y+p.y);
+            }
+            return res;
+
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+
+        public void set(double x, double y){
+            setX(x);
+            setY(y);
+        }
+
+        @Override
+        public String toString() {
+            return "Position{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+
+
 }
